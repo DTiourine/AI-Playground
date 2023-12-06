87f7a3b3e693d468759e6deaea148ee684daf396
DTiourine <109561478+DTiourine@users.noreply.github.com>
2023-12-05 20:29:49 -0500

    Began FashionMNIST

---
+++ FashionMNIST/fmnist_model.py
@@ -0,0 +1,22 @@
+# Neural Network Architecture
+
+import torch.nn as nn
+class FMNISTModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=2, stride=1, padding=0),
+            nn.MaxPool2d(kernel_size=2, stride=3),
+            nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3, stride=2, padding=2),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+            nn.Flatten(start_dim=1),
+            nn.Linear(in_features=200, out_features=120),
+            nn.Linear(in_features=120, out_features=10),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        x = self.layers(x)
+        return x
