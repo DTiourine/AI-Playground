a456daea7befd86881c410cbb88901890e18b9dd
DTiourine <109561478+DTiourine@users.noreply.github.com>
2023-12-06 22:31:38 -0500

    Built EMNIST First Model, built EMNIST training loop

--- EMNIST/emnist_model.py
+++ EMNIST/emnist_model.py
@@ -4,10 +4,16 @@ import torch.nn.functional as F
 class EMNISTNet(nn.Module):
     def __init__(self):
         super(EMNISTNet, self).__init__()
-        self.layers = nn.Sequential(
-            
-        )
-
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(7 * 7 * 64, 128)
+        self.fc2 = nn.Linear(128, 62)  # EMNIST has 62 classes
     def forward(self, x):
-        # Define forward pass here
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+        x = x.view(-1, 7 * 7 * 64)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
--- EMNIST/emnist_train.py
+++ EMNIST/emnist_train.py
@@ -3,8 +3,11 @@ from torch import nn
 from emnist_model import EMNISTNet
 from emnist_data_loader import train_loader
 
-
+#print(torch.cuda.is_available())
 EMNIST_Model = EMNISTNet()
+
+device = torch.device("cuda")
+EMNIST_Model.to(device)
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(params=EMNIST_Model.parameters(), lr=0.1)
@@ -13,8 +16,18 @@ EPOCHS = 100
 
 for epoch in range(EPOCHS):
 
-    running_loss = 0.0
-    last_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader):
 
-    for batch in train_loader:
-        pass
+        #Forward pass
+        outputs = EMNIST_Model(inputs)
+        loss = loss_fn(outputs, labels)
+
+        #Backward pass and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{EPOCHS}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+
+torch.save(EMNIST_Model.state_dict(), 'emnist_model_state_dict.pth')
