494792e35b24160fe9e80b3356f6e4fddc3aa872
DTiourine <109561478+DTiourine@users.noreply.github.com>
2023-12-10 19:11:06 -0500

    Updated EMNIST Model Architecture Iteration 3

--- EMNIST/src/model.py
+++ EMNIST/src/model.py
@@ -6,17 +6,24 @@ class EMNISTNet(nn.Module):
     def __init__(self):
         super(EMNISTNet, self).__init__()
         self.conv_block_1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=2, stride=2, padding=1),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2),
+            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
+            nn.MaxPool2d(3),
             nn.ReLU()
         )
         self.conv_block_2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(3),
+            nn.ReLU()
+        )
+        self.conv_block_3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=0),
             nn.BatchNorm2d(64),
             nn.MaxPool2d(3),
             nn.ReLU()
         )
+
         self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Linear(256, 62),
@@ -25,6 +32,7 @@ class EMNISTNet(nn.Module):
     def forward(self, x):
         x = self.conv_block_1(x)
         x = self.conv_block_2(x)
+        x = self.conv_block_3(x)
         x = self.classifier(x)
         return x
 
